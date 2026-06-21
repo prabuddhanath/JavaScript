ac50c7239595bf4c67d17f071cad372bed0c2918
prabuddhanath <prabuddhanath@gmail.com>
2026-06-21 14:03:27 +0000

    conversion and comparision

---
+++ 01_Basics/03_Conversion_operation.js
@@ -0,0 +1,27 @@
+let score = "33ab"
+
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+let score1 = "33abc"
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1)
+
+str1 = "Hello"
+str2 = " Pravu"
+str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + "2")
+console.log("2" + 1)
+console.log("2" + 1 + 2)
+console.log(2 + 1 + "2")
+
+console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log("2" > 1)
+console.log("1" > 2)
+console.log("10" > 2)
+console.log("prabuddha" > 1)
+
+console.log("=======================")
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log("=======================")
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
